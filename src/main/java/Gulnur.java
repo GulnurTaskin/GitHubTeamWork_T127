e127b919fe13921e4a0bb50ef984cdf5c1f10601
Gulnur <taskin.gulnur@gmail.com>
2023-12-14 09:43:28 +0100

    first commit

---
+++ src/main/java/Gulnur.java
@@ -0,0 +1,14 @@
+public class Gulnur {
+    public static void main (String[]args){
+        System.out.println("gulnur'un yeri");
+
+        /*
+        git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/GulnurTaskin/GitHubTeamWork_T127.git
+git push -u origin main
+         */
+    }
+}
